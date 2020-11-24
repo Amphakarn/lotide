4e7175c01671e61e36c5a9279857111878b1f142
Amphakarn <pisuthigomol@yahoo.com>
2020-11-24 02:07:22 +0000

    Wrote custom assertEqual and added emoji

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  const pass = 0x2705;
+  const fail = 0x1F6D1;
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(pass)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(fail)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("L", "A");
+assertEqual(1, -1);
