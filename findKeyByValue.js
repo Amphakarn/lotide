97531edc3919533bdf33f0ce5c3c2d0f05b86ae3
Amphakarn <pisuthigomol@yahoo.com>
2020-11-29 17:39:07 +0000

    Complete letterPositions, begin coding findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+// compare 2 strings and console.log results
+const assertEqual = function(actual, expected) {
+  const pass = 0x2705;
+  const fail = 0x1F6D1;
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(pass)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(fail)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,42 @@
+// compare 2 arrays and return boolean
+const eqArrays = function(arrA, arrB) {
+  if (arrA.length === arrB.length) {
+    for (let i in arrA) {
+      if (arrA[i] !== arrB[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// receive 2 array, call the eqArrays callback function and console.log results
+const assertArraysEqual = function(arrA, arrB) {
+  const pass = 0x2705;
+  const fail = 0x1F6D1;
+  if (eqArrays(arrA, arrB)) {
+    console.log(`${String.fromCodePoint(pass)} Assertion Passed: ${arrA} === ${arrB}`);
+  } else {
+    console.log(`${String.fromCodePoint(fail)} Assertion Failed: ${arrA} !== ${arrB}`);
+  }
+};
+
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+    for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }      
+    }
+  }
   return results;
 };
 
-const findCoffee = function(beans) {
-  beans.forEach(grind);
-};
-console.log(findCoffee('java'));
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [2]); // Should fail
+assertArraysEqual(letterPositions("Hello").e, [1]); // Should pass
