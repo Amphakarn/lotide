7f686095ed5a2b94310c61e79cf27b37b655884b
Amphakarn <pisuthigomol@yahoo.com>
2020-11-30 23:08:43 +0000

    write test case using mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,30 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1,2]), []); // should pass
-assertArraysEqual(middle([1,2,3,4,5]), [3]); // should pass
-assertArraysEqual(middle([1,2,3,4]), [2, 3]); // should pass
-assertArraysEqual(middle(["A","B",null]), ["B", "C"]); // should fail
+// assertArraysEqual(middle([1,2]), []); // should pass
+// assertArraysEqual(middle([1,2,3,4,5]), [3]); // should pass
+// assertArraysEqual(middle([1,2,3,4]), [2, 3]); // should pass
+// assertArraysEqual(middle(["A","B",null]), ["B", "C"]); // should fail
+
+const assert =  require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("return empty array for [1,2]", () => {
+    const expected = [];
+    const actual = middle([1,2]);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("return [3] for [1,2,3,4,5]", () => {
+    const expected = [3];
+    const actual = middle([1,2,3,4,5]);
+    assert.deepEqual(actual, expected);
+  });
+
+  it("return [2, 3] for 1,2,3,4]", () => {
+    const expected = [2, 3];
+    const actual = middle([1,2,3,4]);
+    assert.deepEqual(actual, expected);
+  });
+});
