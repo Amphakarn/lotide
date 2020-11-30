3a6b990e1f58568abe8d3e3b8f508f34eafd08ca
Amphakarn <pisuthigomol@yahoo.com>
2020-11-30 21:43:44 +0000

    clear lint and adjusted tab size to 2

--- test/headTest.js
+++ test/headTest.js
@@ -2,19 +2,19 @@ const assert = require('chai').assert;
 const head = require('../head');
 
 describe("#head", () => {
-    it("return 1 for [1, 2, 3]", () => {
-        assert.strictEqual(head([1, 2, 3]), 1);
-    });
+  it("return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-    it("return '5' for ['5']", () => {
-        assert.strictEqual(head(['5']), '5');
-    });
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-    it("return 'Hello' for 'Hello', 'Lighthouse', 'Labs'", () => {
-        assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-    });
+  it("return 'Hello' for 'Hello', 'Lighthouse', 'Labs'", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
 
-    it("return undefined for []", () => {
-        assert.strictEqual(head([]), undefined);
-    });
+  it("return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
 });
