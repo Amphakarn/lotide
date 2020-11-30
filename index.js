cdc3cfc7e6545b6ef1a9991167e45621b0131e0e
Amphakarn <pisuthigomol@yahoo.com>
2020-11-30 23:47:19 +0000

    set up index file and export all utility functions EXCEPT assert... functions and all test files in the test folder(may have to do later if required)

---
+++ index.js
@@ -0,0 +1,30 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects
+};
