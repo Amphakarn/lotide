baba23797f1b387e56e03afb52b5ff4106cb264b
Amphakarn <pisuthigomol@yahoo.com>
2020-12-01 01:26:06 +0000

    added all assert functions to index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
-const assertArraysEqual = require('./assertArraysEqual')
-const assertEqual = require('./assertEqual')
-const assertObjectsEqual = require('./assertObjectsEqual')
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head,
-  tail,
-  middle,
-  map,
-  takeUntil,
-  without,
-  findKey,
-  findKeyByValue,
-  letterPositions,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
   countLetters,
   countOnly,
   eqArrays,
   eqObjects,
-  assertArraysEqual,
-  assertEqual,
-  assertObjectsEqual
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
